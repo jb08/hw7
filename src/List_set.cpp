db8c46717a2268a7eee996ec2a61444f4ce041e1
Jesse A. Tov <jesse.tov@gmail.com>
2016-02-22 09:04:29 -0600

    [REDACTED]

--- src/List_set.cpp
+++ src/List_set.cpp
@@ -4,16 +4,14 @@ namespace list_set {
 
 struct List_set::Node
 {
-    element_t element;
-    link_t    next;
-
-    Node(element_t e, link_t n) : element{e}, next{n} { }
+    // YOU NEED TO COMPLETE THIS DEFINITION
 };
 
-List_set::List_set()
-        : link_{ std::make_shared<Node>(element_t{}, nullptr) }
-{ }
-
+// Constructs a `List_set` from an initializer list by creating an empty set
+// and then inserting each element of the initializer list. For example:
+//
+//     List_set abc{"a", "b", "c"};
+//
 List_set::List_set(const std::initializer_list<element_t>& elements)
         : List_set{}
 {
@@ -21,44 +19,9 @@ List_set::List_set(const std::initializer_list<element_t>& elements)
         insert(element);
 }
 
-bool List_set::is_empty() const
-{
-    return link_->next == nullptr;
-}
+// Provide your definitions of the default constructor and five member
+// functions (is_empty, size, member, insert, remove) here.
 
-size_t List_set::size() const
-{
-    size_t result = 0;
-
-    for (auto ptr = link_->next; ptr != nullptr; ptr = ptr->next)
-        ++result;
-
-    return result;
-}
-
-bool List_set::contains(const element_t& element) const
-{
-    for (auto ptr = link_->next; ptr != nullptr; ptr = ptr->next)
-        if (element == ptr->element) return true;
-
-    return false;
-}
-
-void List_set::insert(element_t element)
-{
-    if (contains(element)) return;
-    link_->next = std::make_shared<Node>(element, link_->next);
-}
-
-void List_set::remove(const element_t& element)
-{
-    for (auto ptr = link_; ptr->next != nullptr; ptr = ptr->next) {
-        if (ptr->next->element == element) {
-            ptr->next = ptr->next->next;
-            return;
-        }
-    }
-}
 
 } // end namespace list_set
 
